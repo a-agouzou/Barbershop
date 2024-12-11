6c8b91da3b6492d741f6a5b5abdb7fc0a62ef860
abdellah agouzoul <aagouzou@student.1337.ma>
2024-12-11 19:27:19 +0100

    add a button to list appointments

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,6 +1,8 @@
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 const FooterSection = () => {
+  const navigate = useNavigate();
   return (
     <footer className="bg-white w-screen overflow-hidden border-t-4 border-x-gray-500 p-7 flex justify-between items-center">
       <div className="flex space-x-4">
@@ -17,6 +19,10 @@ const FooterSection = () => {
       <div className="text-black font-bold text-xl md:text-2xl">
         <span className="text-[#FCA95D]">Barber</span>Shop
       </div>
+      {/*button to navigate to appointments lits */}
+      <button className="bg-[#FCA95D] text-black py-2 px-6 rounded-lg hover:bg-white hover:text-black transition-all duration-300" onClick={()=>navigate("/appointments")}>
+        Appointments
+      </button>
     </footer>
   );
 };
